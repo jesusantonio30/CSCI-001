e2e3557796fbebf3278f3753b6c05c2fdf00bf98
Jesus Morales <morales.jesusant@gmail.com>
2025-09-27 22:19:03 -0700

    WIP: Need to revise practice test

---
+++ Test1/practiceTest/practiceTest2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int factorial(int num);
+
+int main() {
+
+    int t, n;
+    double sum = 1;
+
+    cout << "Enter for t: ";
+    cin >> t;
+
+    cout << "Enter for n: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        sum += ( pow(t, i)/ factorial(i) );
+    }
+
+    cout << "e^" << t << " = " << fixed << setprecision(3) << sum << endl;
+
+    
+    return 0;
+}
+
+int factorial(int num) {
+
+    int sum = 1;
+
+    for (int i = num; i >= 1; i--) {
+        sum *= i;
+    }
+    return sum;
+}
