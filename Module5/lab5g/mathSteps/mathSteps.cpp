5c4daab87287801c82678bdc7ca08abe1ab64a15
Jesus Morales <morales.jesusant@gmail.com>
2025-10-01 17:18:46 -0700

    Completed Math Steps assignment in class

---
+++ Module5/lab5g/mathSteps/mathSteps.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float a, b, c;
+
+    cout << "Enter a: ";
+    cin >> a;
+
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << "Enter c: ";
+    cin >> c;
+
+    if (b < 1) {
+        cout << "Your equation: " << a << "x - " << -1 * b << " = " << c << endl;
+    } else {
+        cout << "Your equation: " << a << "x + " << b << " = " << c << endl;
+    }
+
+    cout << "First step: " << a << "x = " << c - b << endl;
+
+    cout << "Second step: " << "x = " << (c - b) / a << endl;
+
+    return 0;
+}
