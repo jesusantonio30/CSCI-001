a1f54d016093f5d28bdca55f6a9dbbc523d070a5
Jesus Morales <morales.jesusant@gmail.com>
2025-10-01 18:37:49 -0700

    Completed PI Infinite Series assignment

--- Module5/lab5e/PiInfiniteSeries.cpp
+++ Module5/lab5e/PiInfiniteSeries.cpp
@@ -6,9 +6,28 @@ using namespace std;
 double calculatePI( int numTerms );
 
 int main() {
+
+    // would become infinite if set equal to 3.14159
+    const double PI = 3.14158;
+
+    double pi = 0;
+    int term = 0;
+
+    while (pi <= PI) {
+        pi = calculatePI(term);
+        cout << "CalculatePI(" << term << ") = " << pi << endl;
+        term += 10;
+    }
+
     return 0;
 }
 
 double calculatePI( int numTerms ){
+    double sum = 0.0;
 
+    for (int i = 0; i < numTerms; i++) {
+        sum += 4 * pow(-1, i) / (2 * i + 1);
+    }
+
+    return sum;
 }
