cf23d28250e9e136965cfd47a31d42a95bedaf61
Jesus Morales <morales.jesusant@gmail.com>
2025-10-01 17:03:21 -0700

    Finished class assignment

--- Module6/inClass/arrToFunc/arrToFunc.cpp
+++ Module6/inClass/arrToFunc/arrToFunc.cpp
@@ -3,25 +3,53 @@
 
 using namespace std;
 
-void printIntArr(double arr[], const int size);
+void printIntArr(float arr[], const int size);
+float averageArray(float arr[], const int size);
+float stdArr(float arr[], const int size);
 
 int main() {
 
-    const int n = 50;
-    double arr[n];
+    const int n = 10;
+    float arr[n];
 
     for (int i = 0; i < n; i++) {
-        arr[i] = sqrt(i);
+        arr[i] = i * i;
     }
 
 
     printIntArr(arr, n);
+    cout << "Average of array: " << averageArray(arr, n) << endl;
+    cout << "Standard Deviation: " << stdArr(arr, n) << endl;
 
     return 0;
 }
 
-void printIntArr(double arr[], const int size) {
+void printIntArr(float arr[], const int size) {
     for (int i = 0; i < size; i++) {
         cout << "Element " << i << ": " << arr[i] << endl;
     }
+}
+
+float averageArray(float arr[], const int size){
+
+    float sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum = sum + arr[i];
+    }
+
+    return sum / size;
+}
+
+float stdArr(float arr[], const int size){
+
+    int sum = 0;
+
+    float avg = averageArray(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        sum += pow(arr[i] - avg, 2);
+    }
+
+    return sqrt( sum / (size - 1));
 }
