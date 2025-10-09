894fe4117e147fbd03aee56d23d4e0157bd6aeac
Jesus Morales <morales.jesusant@gmail.com>
2025-10-08 19:18:42 -0700

    This file was only used for examples so i deleted the contents

--- Module6/Lab6b/averageArrEle.cpp
+++ Module6/Lab6b/averageArrEle.cpp
@@ -3,31 +3,7 @@
 
 using namespace std;
 
-std::string myFunction(int n);
-
-
-string getWord(int n);
-
 int main() {
-int i = 6;
-while (i != 0) {
-cout << getWord(i) << "\t";
-i = i - 2;
-}
-    return 0;
-}
 
-string getWord(int n) {
-if (n == 0)
-return("Labs");
-else if (n == 1)
-return("Sleep");
-else if (n == 2)
-return("Brings");
-else if (n == 3)
-return("Ugly");
-else if (n == 4)
-return("Joy");
-else
-return("C++");
+    return 0;
 }
