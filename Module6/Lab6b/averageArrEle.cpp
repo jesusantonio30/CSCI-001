23610cca3e1eb5863ded4ea1b1285cc5c2bde44b
Jesus Morales <morales.jesusant@gmail.com>
2025-10-11 19:37:27 -0700

    Completed Lab 6b Average Array Elements

--- Module6/Lab6b/averageArrEle.cpp
+++ Module6/Lab6b/averageArrEle.cpp
@@ -34,6 +34,7 @@ int main() {
     return 0;
 }
 
+// Switched parameter to pointer to slowly get the hang of it
 double average( double *a, const int size) {
     double sum = 0.0;
     for (int i = 0; i < size; i++){
