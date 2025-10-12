76af7002a38e90c04a47fd50afb31877049125f0
Jesus Morales <morales.jesusant@gmail.com>
2025-10-11 19:34:01 -0700

    WIP: Working on Lab 6b. Implementing a function to find the average of values in an array

--- Module6/Lab6b/averageArrEle.cpp
+++ Module6/Lab6b/averageArrEle.cpp
@@ -3,7 +3,43 @@
 
 using namespace std;
 
+double average( double *a, const int size);
+
 int main() {
 
+    const int size = 5;
+    double data[size];
+
+    cout << "\n---------- Find The Average of Five Values ----------\n" << endl;;
+
+    int i;
+
+    for (i = 0; i < size; i++) {
+        cout << "Enter value " << i + 1 << ": " ;
+        cin >> data[i];
+    }
+
+    cout << "\nYour values are: ";
+
+    for (i = 0; i < size; i++) {
+        if (i != size - 1 ){
+            cout << data[i] << ", ";
+        } else {
+            cout << data[i] << endl;
+        }
+    }
+
+    cout << "And the average is: " << average(data, size) << '\n' << endl;
+
     return 0;
+}
+
+double average( double *a, const int size) {
+    double sum = 0.0;
+    for (int i = 0; i < size; i++){
+        sum += a[i];
+    }
+    double avg = sum / size;
+
+    return avg;
 }
