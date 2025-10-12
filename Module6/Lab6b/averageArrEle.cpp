ac5749ed5597ea181db0ebeb8ca2cfeac00eca2e
Jesus Morales <morales.jesusant@gmail.com>
2025-10-11 19:45:38 -0700

    Forgot to add the do while loop to Lab 6b

--- Module6/Lab6b/averageArrEle.cpp
+++ Module6/Lab6b/averageArrEle.cpp
@@ -9,27 +9,34 @@ int main() {
 
     const int size = 5;
     double data[size];
+    char runAgain;
 
     cout << "\n---------- Find The Average of " << size << " Values ----------\n" << endl;;
 
     int i;
 
-    for (i = 0; i < size; i++) {
-        cout << "Enter value " << i + 1 << ": " ;
-        cin >> data[i];
-    }
-
-    cout << "\nYour values are: ";
-
-    for (i = 0; i < size; i++) {
-        if (i != size - 1 ){
-            cout << data[i] << ", ";
-        } else {
-            cout << data[i] << endl;
+    do {
+        for (i = 0; i < size; i++) {
+            cout << "Enter value " << i + 1 << ": " ;
+            cin >> data[i];
         }
-    }
 
-    cout << "And the average is: " << average(data, size) << '\n' << endl;
+        cout << "\nYour values are: ";
+
+        for (i = 0; i < size; i++) {
+            if (i != size - 1 ){
+                cout << data[i] << ", ";
+            } else {
+                cout << data[i] << endl;
+            }
+        }
+
+        cout << "And the average is: " << average(data, size) << '\n' << endl;
+
+        cout << "\n\nWant to run again? (y/n): ";
+        cin >> runAgain;
+        
+    } while (runAgain == 'y');
 
     return 0;
 }
