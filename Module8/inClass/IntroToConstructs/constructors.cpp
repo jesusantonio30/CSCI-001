f7db3fa27d73d9939f97e9fa2830be038cd8ed6d
Jesus Morales <morales.jesusant@gmail.com>
2025-11-05 17:56:04 -0800

    Added upon constructors class assignment. Created a triangle struct and composed Point structs into it

--- Module8/inClass/IntroToConstructs/constructors.cpp
+++ Module8/inClass/IntroToConstructs/constructors.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 struct Point {
@@ -8,11 +9,35 @@ struct Point {
     Point(double mA = 0.0, double mB = 0.0);
 };
 
+struct Triangle {
+    Point a, b, c;
+
+    Triangle(Point mA = {}, Point mB = {}, Point mC = {});
+    void print();
+};
+
+double distance(Point pA, Point pB);
+
 int main() {
+        //  x1 y2    x2  y2
+    Point a(-2, 1), b(3, 5);
 
-    Point pt;
+    a.print();
+    b.print();
 
-    pt.print();
+    cout << "Distance between: " << distance(a, b) << endl;
+
+
+    Point A(-2, 1), B(3,5), C(2,1);
+    Triangle y(Point(-2,1), Point(3,5), Point(2,1));
+    Triangle x(A,B,C);
+    x.print();
+
+    cout << "Distance between A & B (two point structs): " << distance(A, B) << endl;
+    cout << "Distance between A & B: " << distance(x.a, x.b) << endl;
+    cout << "Distance between B & C: " << distance(x.b, x.c) << endl;
+    cout << "Distance between A & C: " << distance(x.a, x.c) << endl;
+
 
     return 0;
 }
@@ -24,4 +49,20 @@ Point::Point(double mA, double mB) {
 
 void Point::print() {
     cout << "( " << a << ", " << b << " )" << endl;
+}
+
+double distance(Point pA, Point pB) {
+    return sqrt( pow(pB.a - pA.a, 2) + pow(pB.b - pA.b, 2));
+}
+
+Triangle::Triangle(Point mA, Point mB, Point mC){
+    a = mA;
+    b = mB;
+    c = mC;
+}
+void Triangle::print() {
+    cout << "Triangle formed by: " << endl;
+    a.print();
+    b.print();
+    c.print();
 }
