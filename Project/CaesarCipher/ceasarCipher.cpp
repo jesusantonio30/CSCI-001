c4c9e88c52103062b5728d4b6eec4f5a370242d6
Jesus Morales <morales.jesusant@gmail.com>
2025-11-22 09:46:18 -0800

    Created files for Caesar Cipher project

---
+++ Project/CaesarCipher/ceasarCipher.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    /*
+        class
+        message
+        key
+        constructor -> include defaults
+
+        getters and setters
+
+        encrypt and decrypt
+        lower case
+    */
+
+    return 0;
+}
